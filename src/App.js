86925934716a347becc73176b7b95ccbf8714289
Jainam Mehta <jainamm47@gmail.com>
2020-10-03 12:18:19 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Album from './components/Album/Album';
 import PicContainer from './components/PicContainer/PicContainer';
-import Title from './components/Title';
 
 class App extends Component {
 
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Title} />
         <Route exact path="/" component={Album} />
         <Route path="/image/:id" component={PicContainer} />
       </Router>
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,7 +6,7 @@ class Item extends Component {
   render() {
     return (
       <div style={{overflow: 'hidden'}} className="itemStyle">
-        <img  src={this.props.picture.url} alt="" width="100%"></img>
+        <img src={this.props.picture.url} alt="" width="100%"></img>
       </div>
     );
   }
